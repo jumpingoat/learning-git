a35fb7dba771610862a576c5ec851a9652239f6f
kozinek <alklonth@gmail.com>
2024-10-22 18:14:13 +0200

    Init project

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ dict = {
 
 liczba = 0
 for sklep, zakupy in dict.items():
-    print(f"Idę do {sklep.capitalize()} i kupuję tam {zakupy}")
+    print(f"Idę do {sklep.capitalize()} i kupuję tam {zakupy.capitalize()}")
     liczba += len(zakupy)
 print(f"W sumie kupuję {liczba} rzeczy.")
 
